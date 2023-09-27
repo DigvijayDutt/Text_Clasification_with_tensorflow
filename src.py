c26acf1a7c23ef73e0d315ccdf1dee899b3f4535
DigvijayDutt <duttdigvijay@gmail.com>
2023-09-27 14:26:48 +0530

    Final code for making text classifier.

--- src.py
+++ src.py
@@ -7,3 +7,26 @@ train_data, validation_data, test_data = tfds.load(
     split=('train[:60%]', 'train[60%:]', 'test'),
     as_supervised=True
 )
+train_example_batch, train_labels_batch = next(iter(train_data.batch(10))) #training
+#embedding 
+embedding = "https://tfhub.dev/google/tf2-preview/gnews-swivel-20dim/1"
+hub_layer=hub.KerasLayer(embedding, input_shape = [], dtype=tf.string, trainable=True)
+hub_layer(train_example_batch[:3])
+
+#creating model
+model = tf.Keras.Sequential()
+model.add(hub_layer)
+
+#output density layer
+model.add(tf.keras.layers.Dense(16, activation='relu'))
+model.add(tf.keras.layers.Dense(1))
+
+#compile model
+model.compile(optimizer='adam', loss=tf.keras.losses.BinaryCrossentropy(from_logits=True), metrics=['accuracy'])
+
+#history of model
+history = model.fit(train_data.shuffle(10000).batch(100), epochs = 25, validation_data = validation_data.batch(100), verbose=1)
+
+results = model.evaluate(test_data.batch(100), verbose=2)
+for name, values in zip(model.metrics_names, results):
+    print("%s: %.3f" % (name, values))
